edd953c38fc0a034878cb66a59f57c962d8c669a
Caknoooo <caknocomel@gmail.com>
2024-01-05 19:38:13 +0700

    feat: initial commit

---
+++ src/test/java/dev/caknoo/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.caknoo.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
